302b73e95e368da44bb32f6fb2f4e49984e939b7
RQ <mengmengmengqiang@gmail.com>
2023-06-11 23:56:47 +0800

    feat: rename completion db to postgrelsql db

--- db/postgresql.go
+++ db/postgresql.go
@@ -10,13 +10,13 @@ func Init() {
 	supabaseUrl := "https://sfisgjpeqptcluzmtbup.supabase.co"
 	supabaseKey := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmaXNnanBlcXB0Y2x1em10YnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM5MzAyMzAsImV4cCI6MTk5OTUwNjIzMH0.eHhleg3ev4YGA1yHosohWwzxOZxNEh4hP1PavfMF-X0"
 	supabase := supa.CreateClient(supabaseUrl, supabaseKey)
-	completionDB = supabase.DB
+	userDB = supabase.DB
 }
 
 // completion DB
-var completionDB *postgrest.Client
+var userDB *postgrest.Client
 
 // CompletionDB completion DB
 func CompletionDB() *postgrest.Client {
-	return completionDB
+	return userDB
 }
