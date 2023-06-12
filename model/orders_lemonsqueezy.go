192ba7500fe178449449ac1d4cf0bd01a018097f
RQ <mengmengmengqiang@gmail.com>
2023-06-12 13:08:51 +0800

    feat: fix sql table

--- model/orders_lemonsqueezy.go
+++ model/orders_lemonsqueezy.go
@@ -1,1 +1,19 @@
 package model
+
+import "time"
+
+type LemonSqueezyOrder struct {
+	ID              uint64    `gorm:"column:id"`
+	OrderID         uint64    `gorm:"column:order_id"`
+	UID             string    `gorm:"column:uid"`
+	StoreID         uint64    `gorm:"column:store_id"`
+	Identifier      string    `gorm:"column:identifier"`
+	Status          string    `gorm:"column:status"`
+	ProductID       uint64    `gorm:"column:product_id"`
+	VariantID       uint64    `gorm:"column:variant_id"`
+	ProductName     string    `gorm:"column:product_name"`
+	VariantName     string    `gorm:"column:variant_name"`
+	OrderCreateTime time.Time `gorm:"column:order_create_time"`
+	CreatedAt       time.Time `gorm:"column:created_at"`
+	UpdatedAt       time.Time `gorm:"column:updated_at"`
+}
