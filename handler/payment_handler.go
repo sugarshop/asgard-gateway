9b6f9b1ebdf29003be1df6d3d51f7eeb9f73dada
RQ <mengmengmengqiang@gmail.com>
2023-06-14 00:48:09 +0800

    feat: add payment handler

---
+++ handler/payment_handler.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sugarshop/asgard-gateway/service"
+)
+
+type PaymentHandler struct {
+}
+
+// NewPaymentHandler return payment handler
+func NewPaymentHandler() *PaymentHandler {
+	return &PaymentHandler{}
+}
+
+func (h *PaymentHandler) Register(e *gin.Engine) {
+	e.POST("/v1/payment/lemonsqueezy/checkout", JSONWrapper(h.LemonSqueezy))
+}
+
+// LemonSqueezy create lemonsqueezy checkout link
+func (s *PaymentHandler) LemonSqueezy(c *gin.Context) (interface{}, error) {
+	ctx := context.Background()
+	link, err := service.LemonSqueezyServiceInstance().CreateCheckoutLink(ctx, "uiduiduid")
+	if err != nil {
+		return nil, err
+	}
+	return map[string]interface{}{
+		"link": link,
+	}, nil
+}
